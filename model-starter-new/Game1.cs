696d6542825bc2d1c5d32da0130169a51459a948
Gage <ghawb01@ksu.edu>
2023-04-18 10:06:12 -0500

    All But Pos

--- model-starter-new/Game1.cs
+++ model-starter-new/Game1.cs
@@ -4,46 +4,89 @@ using Microsoft.Xna.Framework.Input;
 
 namespace model_starter_new
 {
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Terrain terrain;
+        ChaseCamera camera;
+        Tank tank;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
         }
 
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            
             base.Initialize();
         }
 
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            camera = new ChaseCamera(this, new Vector3(0, 10, -30));
+            camera.Target = tank;
+            Texture2D heightmap = Content.Load<Texture2D>("heightmap");
+            terrain = new Terrain(this, heightmap, 10f, Matrix.CreateTranslation(-127f, 0, 127));// Matrix.CreateTranslation(-128, 0, -128));            
+            tank = new Tank(this);
+            tank.HeightMap = terrain;
             // TODO: use this.Content to load your game content here
         }
 
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            camera.Update(gameTime);
+            tank.Update(gameTime);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            terrain.Draw(camera);
+            tank.Draw(camera);            
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
---
+++ model-starter-new/IFollowable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace model_starter_new
+{
+        public interface IFollowable
+        {
+            /// <summary>
+            /// The IFollowable's position in the world 
+            /// </summary>
+            Vector3 Position { get; }
+
+            /// <summary>
+            /// The angle the IFollowable is facing, in radians 
+            /// </summary>
+            float Facing { get; }
+        }
+}
